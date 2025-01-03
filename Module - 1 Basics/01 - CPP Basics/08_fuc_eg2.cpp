bbadbdd833114a21fe78f028bc060f9646094b7a
guru <gurusaghi324@gmail.com>
2025-01-03 14:29:09 +0530

    commit

--- Module - 1 Basics/01 - CPP Basics/08_fuc_eg2.cpp
+++ Module - 1 Basics/01 - CPP Basics/08_fuc_eg2.cpp
@@ -1,11 +1,22 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+int maxx (int num1, int num2){
+    if(num1 >= num2) return num1;
+    else return num2;
+}
 
 int main(){
     int num1, num2;
-    cin >> num1 >> num2 ;
+    cin >> num1 >> num2 ; // 5 , 6
+
+    int minimum = min(num1,num2);
+
+    cout << minimum << endl; // 5
     
+    int miximum = maxx(num1,num2);
+
+    cout << miximum << endl; // 6
 
 
     return 0;
