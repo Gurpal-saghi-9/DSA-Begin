8b796292fe5720a2825ce5be9d2f1658f7f9c9f0
guru <gurusaghi324@gmail.com>
2025-01-03 13:48:15 +0530

    commit

---
+++ Module - 1 Basics/01 - CPP Basics/06_for_loop.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+    // cout << "Gurpal singh" << endl;
+    // cout << "Gurpal singh" << endl;
+    // cout << "Gurpal singh" << endl;
+    // cout << "Gurpal singh" << endl;
+    // cout << "Gurpal singh" << endl;
+
+    // instead above
+    // using for loop for iteration
+
+    int i;
+    for (i = 0; i <= 10 ; i++) // execute 10 times increasing order
+    {
+        cout << "Gurpal singh " << i << endl;
+    }
+    cout << i << endl; // 11
+
+    int j;
+    for (j = 5; j > 0 ; j--) // execute 5 times decreasing order 
+    {
+        cout << "Guru " << j << endl;
+    }
+    cout << j << endl; //0
+
+    return 0;
+}
