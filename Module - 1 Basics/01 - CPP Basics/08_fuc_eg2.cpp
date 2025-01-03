18d0b654a555420a5628223608249d94a0a21b51
guru <gurusaghi324@gmail.com>
2025-01-03 14:22:04 +0530

    comit

---
+++ Module - 1 Basics/01 - CPP Basics/08_fuc_eg2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+int main(){
+    int num1, num2;
+    cin >> num1 >> num2 ;
+    
+
+
+    return 0;
+}
--- Module - 1 Basics/01 - CPP Basics/08_func_eg1.cpp
+++ Module - 1 Basics/01 - CPP Basics/08_func_eg1.cpp
@@ -18,7 +18,7 @@ int main(){
     int num1, num2;
     // int sum;
 
-    // cin >> num1 >> num2 ;
+    cin >> num1 >> num2 ;
     // sum = num1 + num2;
 
     // int res = add(num1, num2);
