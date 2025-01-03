561f093a118a4706e96c01eee2e2388024d60ef0
guru <gurusaghi324@gmail.com>
2025-01-03 13:53:52 +0530

    commit

---
+++ Module - 1 Basics/01 - CPP Basics/07_while_loop.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(){
+    // cout << "Gurpal singh" << endl;
+    // cout << "Gurpal singh" << endl;
+    // cout << "Gurpal singh" << endl;
+    // cout << "Gurpal singh" << endl;
+    // cout << "Gurpal singh" << endl;
+
+    // instead above
+    // using while loop for iteration
+
+    int i = 0;
+    while(i <= 10) // execute 10 times increasing order
+    {
+        cout << "Gurpal singh " << i << endl;
+        i++;
+    }
+    cout << i << endl; // 11
+
+    int j = 5;
+    while (j > 0) // execute 5 times decreasing order 
+    {
+        cout << "Guru " << j << endl;
+        j--;
+    }
+    cout << j << endl; //0
+
+    return 0;
+}
