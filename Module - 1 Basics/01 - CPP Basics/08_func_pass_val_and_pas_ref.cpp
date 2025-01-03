c92ac81c47555e4eb85cfd33ea8b0e4b1b377e30
guru <gurusaghi324@gmail.com>
2025-01-03 14:43:07 +0530

    commit

--- Module - 1 Basics/01 - CPP Basics/08_func_pass_val_and_pas_ref.cpp
+++ Module - 1 Basics/01 - CPP Basics/08_func_pass_val_and_pas_ref.cpp
@@ -1,9 +1,36 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// pass by value 
+// void doSomthing(int num){
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+//     num += 5;
+//     cout << num << endl;
+
+// }
+
+// pass by reference
+void doSomthing(int &num){
+    cout << num << endl;
+    num += 5;
+    cout << num << endl;
+    num += 5;
+    cout << num << endl;
+
+}
 
 int main(){
-    int numm = 10;
+    int num = 10;
+
+    // pass by value
+    // doSomthing(num);
+    // cout<< num << endl; // 10
+
+    // pass by reference 
+    doSomthing(num);
+    cout<< num << endl; // 20
 
 
     return 0;
