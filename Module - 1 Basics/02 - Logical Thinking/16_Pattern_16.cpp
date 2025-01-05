a496e85723eed8484c29c9f0b57e0a3b9cd9ca92
guru <gurusaghi324@gmail.com>
2025-01-05 10:25:46 +0530

    commit

--- Module - 1 Basics/02 - Logical Thinking/16_Pattern_16.cpp
+++ Module - 1 Basics/02 - Logical Thinking/16_Pattern_16.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printPattern9(int n){
+void printPattern16(int n){
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
     
 }
 
 int main(){
-    int t;
-    cin >> t;
-
-    for (int i = 0; i < t; i++){
         int n;
         cin >> n;
-        printPattern9(n);
-    }
+        printPattern16(n);
 
     return 0;
     
