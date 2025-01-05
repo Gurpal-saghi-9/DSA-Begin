2eed7f1165a235c63d05b71d21deb7809f139dec
guru <gurusaghi324@gmail.com>
2025-01-05 11:04:57 +0530

    commit

--- Module - 1 Basics/02 - Logical Thinking/20_Pattern_20.cpp
+++ Module - 1 Basics/02 - Logical Thinking/20_Pattern_20.cpp
@@ -1,19 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printPattern9(int n){
+void printPattern20(int n){
+    int space = 2*n-2;
+    for (int i = 1; i <= 2*n-1; i++)
+    {
+        int star = i;
+        if (i > n) star = 2*n-i;
+        for (int j = 1; j <= star; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= star; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        if (i < n) space -= 2;
+        else space += 2; 
+    }
+    
     
 }
 
 int main(){
-    int t;
-    cin >> t;
-
-    for (int i = 0; i < t; i++){
         int n;
         cin >> n;
-        printPattern9(n);
-    }
+        printPattern20(n);
 
     return 0;
     
