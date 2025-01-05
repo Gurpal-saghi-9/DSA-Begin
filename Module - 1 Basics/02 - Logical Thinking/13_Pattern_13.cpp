7309167cb4fc36980545b07b2c60eb7c8438c917
guru <gurusaghi324@gmail.com>
2025-01-05 10:13:37 +0530

    commit

--- Module - 1 Basics/02 - Logical Thinking/13_Pattern_13.cpp
+++ Module - 1 Basics/02 - Logical Thinking/13_Pattern_13.cpp
@@ -1,19 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printPattern9(int n){
+void printPattern13(int n){
+    int num = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << num << " ";
+            num += 1;
+        }
+        cout << endl;
+    }
     
 }
 
 int main(){
-    int t;
-    cin >> t;
-
-    for (int i = 0; i < t; i++){
         int n;
         cin >> n;
-        printPattern9(n);
-    }
+        printPattern13(n);
 
     return 0;
     
