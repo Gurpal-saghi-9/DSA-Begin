9163b132a0aca4a30585a61aa6a4915739c37798
guru <gurusaghi324@gmail.com>
2025-01-05 11:09:46 +0530

    commit

--- Module - 1 Basics/02 - Logical Thinking/21_Pattern_21.cpp
+++ Module - 1 Basics/02 - Logical Thinking/21_Pattern_21.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 void printPattern21(int n){
-    
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j< n; j++)     
+        {
+            if (i == 0 || j == 0 || i == n-1 || j == n-1){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;   
+    }
 }
 
 int main(){
