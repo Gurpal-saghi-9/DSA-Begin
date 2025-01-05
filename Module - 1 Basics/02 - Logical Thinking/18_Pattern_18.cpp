9f96536d8397f6189e761e005f9db4b85e2d3187
guru <gurusaghi324@gmail.com>
2025-01-05 10:42:15 +0530

    commit

--- Module - 1 Basics/02 - Logical Thinking/18_Pattern_18.cpp
+++ Module - 1 Basics/02 - Logical Thinking/18_Pattern_18.cpp
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printPattern9(int n){
+void printPattern18(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'E' - i ; ch <= 'E'; ch++)        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
     
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = ('A'+ n - 1)-i; ch <= ('A' + n - 1); ch++)        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main(){
-    int t;
-    cin >> t;
-
-    for (int i = 0; i < t; i++){
         int n;
         cin >> n;
-        printPattern9(n);
-    }
+        printPattern18(n);
 
     return 0;
     
