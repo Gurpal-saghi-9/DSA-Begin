30f93b1eb39d9103da4c6fc1e1aadc9c68cadb21
guru <gurusaghi324@gmail.com>
2025-01-07 10:51:24 +0530

    commit

---
+++ Module - 1 Basics/04 - Basic Maths/06_Prime.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void brute(int n){
+    int count = 0;
+
+    for (int i = 1; i<= n; i++)
+    {
+        if(n%i==0){
+            count += 1;
+        }
+    }
+    if (count == 2)
+    {
+        cout << "prime" << endl;
+    }
+    else{
+        cout << "not prime" << endl;
+    }
+}
+
+void optimal(int n){
+    int count = 0;
+
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0){
+            count += 1;
+            if(n / i != i){
+                count += count;
+            }
+        }
+    }
+    if (count == 2){
+        cout << n << " is prime number" << endl;
+    }
+    else{
+        cout << n << " is not prime number" << endl;
+    }
+    
+}
+
+int main(){
+    int n ;
+    cin >> n;
+
+    brute(n);
+
+    optimal(n);
+    
+    
+    return 0;
+}
